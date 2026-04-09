00580cfb7c08307283b01c6987cfe373cbbd28d6
GauraviSaxena <2300301540040@ipec.org.in>
2026-04-09 12:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (96.80%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,61 @@
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) return true;
+//         return Solve(root.left, root.right);
+//     }
+
+//     public boolean Solve(TreeNode t1, TreeNode t2) {
+//         if (t1 == null && t2 == null) return true;
+//         if (t1 == null || t2 == null) return false;
+
+//         return (t1.val == t2.val) &&
+//               Solve(t1.left, t2.right) &&
+//                Solve(t1.right, t2.left);
+//     }
+// }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        if(root == null) return true ;
+
+        return Solve(root.left, root.right) ;
+    }
+
+    public static boolean Solve(TreeNode p , TreeNode q)
+    {
+        if(p == null && q == null) return true ;
+        if(p == null || q == null || p.val != q.val) return false ;
+
+        return Solve(p.left,q.right) && Solve(q.left,p.right) ;
+    }
+}
