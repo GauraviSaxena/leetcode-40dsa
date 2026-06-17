26fc0084fb3bdbb6728aaf9e99cd3ba5d9e4b15a
GauraviSaxena <2300301540040@ipec.org.in>
2026-06-17 10:29:17 +0530

    Time: 307 ms (8.27%) | Memory: 48 MB (41.98%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,14 +1,35 @@
 class Solution {
+
+    Boolean[] dp;
+
     public boolean canJump(int[] nums) {
-      int n=nums.length;
-      int maxJump=0;
-      for(int i=0;i<nums.length;i++)
-      {
-        if(i> maxJump) return false;
 
-        maxJump=Math.max(maxJump, i+nums[i]);
-      }
-      return true;
+        dp = new Boolean[nums.length];
 
+        return solve(nums, 0);
+    }
+
+    public boolean solve(int[] nums, int index) {
+
+        // Last index ya uske aage pahunch gaye
+        if (index >= nums.length - 1) {
+            return true;
+        }
+
+        // Already computed
+        if (dp[index] != null) {
+            return dp[index];
+        }
+
+        // Sab possible jumps try karo
+        for (int jump = 1; jump <= nums[index]; jump++) {
+
+            if (solve(nums, index + jump)) {
+                return dp[index] = true;
+            }
+        }
+
+        // Koi bhi jump successful nahi hua
+        return dp[index] = false;
     }
 }
