576ccd2a034e39f33e659718e333450cbd7583e6
GauraviSaxena <2300301540040@ipec.org.in>
2026-06-18 10:10:08 +0530

    Time: 3 ms (89.01%) | Memory: 47.7 MB (31.55%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,37 +1,32 @@
 class Solution {
-
     public int candy(int[] ratings) {
-
-        int n = ratings.length;
-
-        int[] candies = new int[n];
-
-        Arrays.fill(candies, 1);
-
-        // Left to Right
-        for (int i = 1; i < n; i++) {
-
-            if (ratings[i] > ratings[i - 1]) {
-
-                candies[i] = candies[i - 1] + 1;
+        
+        int n = ratings.length ;
+        int[] Left = new int[n] ;
+        Arrays.fill(Left,1) ;
+        for(int i = 1 ; i < n ;i++)
+        {
+            if(ratings[i] > ratings[i-1])
+            {
+                Left[i] = Left[i-1]+1 ;
             }
         }
 
-        // Right to Left
-        for (int i = n - 2; i >= 0; i--) {
-
-            if (ratings[i] > ratings[i + 1]) {
-
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        int[] Right = new int[n] ;
+        Arrays.fill(Right,1) ;
+        for(int i = n-2 ; i >= 0 ; i--)
+        {
+            if(ratings[i] > ratings[i+1])
+            {
+                Right[i] = Right[i+1]+1 ;
             }
         }
 
-        int sum = 0;
-
-        for (int candy : candies) {
-            sum += candy;
+        int ans = 0 ;
+        for(int i = 0 ; i < n ; i++)
+        {
+            ans += Math.max(Left[i],Right[i]) ;
         }
-
-        return sum;
+        return ans ;
     }
 }
